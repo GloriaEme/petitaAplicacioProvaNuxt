05707948da2da2cc10df9e27a2f3d1c8c77134ba
Gloria Molina <gloriamolina44@gmail.com>
2023-01-18 15:33:20 +0100

    prova

---
+++ codi/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container style="height:100vh" fill-height>
+    <v-row align="center" justify="center" style="height:100%">
+      <v-col align="center">
+        <div style="font-weight:100;font-size:40px;color:white">
+          <strong>En Contrucción</strong> 
+         
+              
+        </div>
+      </v-col>
+    </v-row>
+  </v-container> 
+</template>
